1a3f068a839b11d67204b4b5ab6ac53320388153
Pearce Allen <pearce.p.allen@gmail.com>
2020-12-28 17:00:03 -0500

    Added wiki/title GET links + Markdown rendering

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    return render(request, "encyclopedia/entry.html", {
+        "content": markdown2.markdown(util.get_entry(name)), 
+        "name": name
+    })
